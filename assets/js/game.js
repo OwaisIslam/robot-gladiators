c1ee7dc13fe6b8f5c42a11f22116de8b4241faf3
Owais Islam <owaisislam@gmail.com>
2020-08-10 21:27:38 -0500

    added enemy names

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,7 +5,18 @@ var playerAttack = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+
+console.log(enemyNames.length);
+for (var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -67,4 +78,4 @@ var fight = function () {
     }
 };
 
-fight();
+// fight();
