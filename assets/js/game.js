ceeec74ad18e3e8a82ce64b35c3d7bf9a65e1cad
Owais Islam <owaisislam@gmail.com>
2020-08-10 21:32:50 -0500

    changed function parameters

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,13 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
-
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+var playerMoney = 10;
 
 var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
-console.log(enemyNames);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-
-console.log(enemyNames.length);
-for (var i = 0; i < enemyNames.length; i++) {
-    console.log(enemyNames[i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index");
-}
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+var fight = function (enemyName) {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -78,4 +65,6 @@ var fight = function () {
     }
 };
 
-// fight();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
