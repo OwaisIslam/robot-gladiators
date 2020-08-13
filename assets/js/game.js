54faa132c346b26dc31e459763a10f10956f876b
Owais Islam <owaisislam@gmail.com>
2020-08-12 20:55:38 -0500

    fixed fight/skip response bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,33 @@
+var fightOrSkip = function () {
+    var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+    promptFight = promptFight.toLowerCase();
+
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
+    if (promptFight === "skip" || promptFight === "SKIP") {
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        if (confirmSkip) {
+            window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+
+            playerInfo.money = Math.max(0, playerInfo.money - 10);
+
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
 var fight = function (enemy) {
     while (playerInfo.health > 0 && enemy.health > 0) {
-        var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-
-        if (promptFight === "skip" || promptFight === "SKIP") {
-            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-            if (confirmSkip) {
-                window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-                playerInfo.money = Math.max(0, playerInfo.money - 10);
-                console.log("playerInfo.money", playerInfo.money)
-                break;
-            }
+        if (fightOrSkip()) {
+            break;
         }
 
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
